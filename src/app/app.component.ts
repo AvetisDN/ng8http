2a80a850e50f0d8a676cea8f1c8b2ac70fef69bf
AvetisDN <avetisdn@gmail.com>
2019-08-29 18:41:24 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigService } from './config.service';
+import {Character} from './Character';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng8http';
+export class AppComponent implements OnInit {
+  characters: Character[] = [];
+  constructor(private cs: ConfigService) {
+  }
+  ngOnInit() {
+    this.cs.getCharacters().subscribe((data: Character[]) => {
+      this.characters = data;
+    });
+  }
 }
